0f4da3a0137497fe843b887460609c4e44d5b799
Stephen Leake <stephen_leake@stephe-leake.org>
2021-07-30 02:49:14 -0700

    Allow compiling in elpa workspace
    * build.sh: Simplify finding WISI_DIR.
    
    * ada_mode.prj: New file, for compiling in elpa workspace.
    * prj.el:
    
    * wisi.gpr.gp: Delete; only in packages/wisi.

---
+++ prj.el
@@ -0,0 +1,16 @@
+;; Emacs wisi project definitions for compiling ada-mode in ELPA workspace
+
+(let ((wisi-core (expand-file-name "../wisi")))
+
+  (wisi-prj-select-cache
+   "ada_mode.prj"
+   (wisi-prj-default ;; for ada-build prj vars
+    (create-ada-prj
+    :name "ada_mode elpa"
+    :compile-env
+    (list
+     (concat "WISI=" wisi-core))))
+   "Makefile"
+   ))
+
+;; end of file
